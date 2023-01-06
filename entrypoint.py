039c8d86234b529cca693511a6a56a01f3903fae
GabrielvanderSchmidt <gabriel.vanderschmidt@gmail.com>
2023-01-06 16:56:14 -0300

    Fixed error on entrypoint.py

--- entrypoint.py
+++ entrypoint.py
@@ -13,7 +13,7 @@ check_model = "pspnet_r50-d8_512x1024_40k_cityscapes_20200605_003338-2966598c.pt
 while True:
    for file in os.listdir(os.path.join(root, "original/todo")):
        print(f"{datetime.now().strftime('%Y-%m-%d %H:%M:%S')}\t- Checking file {file}")
-       if file.endswith(".avi") and os.isfile(os.path.join(root, "original/todo", file)):
+       if file.endswith(".avi") and os.path.isfile(os.path.join(root, "original/todo", file)):
            start = datetime.now()
            print(f"{datetime.now().strftime('%Y-%m-%d %H:%M:%S')}\t- Segmenting {os.path.join(root, 'original/todo', file)}...")
            s1 = subprocess.run("python3", os.path.join(segnext, "demo/video_demo.py"), os.path.join(root, "original/todo", file), os.path.join(segnext, "configs", model), os.path.join(segnext, "checkpoints", check_model), '--device="cpu"', f"--output-file={os.path.join(root, 'segmented', file)}")
